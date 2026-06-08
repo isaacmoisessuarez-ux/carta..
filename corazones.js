1c6b02b033fe53b0b013decd18aed9a012550a69
isaacmoisessuarez-ux <isaacmoisessuarez@gmail.com>
2026-06-08 02:12:28 -0500

    Reset audio current time to 0 before playing

--- corazones.js
+++ corazones.js
@@ -33,7 +33,7 @@ let musicaIniciada = false;
 
 function activarMusica() {
   if (!musicaIniciada) {
-    audio.currentTime = 3;
+    audio.currentTime = 0;
     audio.play().catch(error => console.log("El navegador bloqueó el autoplay: ", error));
     musicaIniciada = true;
   }
